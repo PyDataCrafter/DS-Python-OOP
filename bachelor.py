395d7bf0f1daae95eaeb3e07e1ade8e0a6ba2fa4
kiey <joan.marcual93@gmail.com>
2022-06-07 16:29:55 +0200

    initial

---
+++ bachelor.py
@@ -0,0 +1,30 @@
+from excepcionesUniversidad import *
+from persona import Profesor
+
+
+class Bachelor():
+    def __init__(self, nombre):
+        pass
+
+    def anadir_asignatura(self, asignatura):
+        pass
+
+    def eliminar_asignatura(self, asignatura):
+        pass
+
+
+class Asignatura():
+    def __init__(self, nombre, codigo):
+        pass
+
+    def anadir_estudiantes(self, estudiante):
+        pass
+
+    def eliminar_estudiantes(self, estudiante):
+        pass
+
+    def get_numero_estudiantes(self):
+        pass
+
+    def dar_clase(self):
+        """El profesor da clase a todos los estudiantes de la clase"""
---
+++ persona.py
@@ -0,0 +1,14 @@
+class Persona():
+    pass
+
+
+class Estudiante():
+    def estudiar(self):
+        pass
+
+
+class Profesor():
+    pass
+
+    def ensenar(self, estudiante):
+        pass
